5bd0fcb4a9427560f8e1720f821c1df9f3e205ea
bmhcode <bmhwin@gmail.com>
2024-01-05 05:20:41 +0100

    debug=False

--- project/settings.py
+++ project/settings.py
@@ -29,7 +29,7 @@ environ.Env.read_env()
 SECRET_KEY = 'django-insecure-5b)2xi-%m%0i=6ddd_6ugdz2ufzn_18u(&g&(8v@k7gs$n14_j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 # ALLOWED_HOSTS = ['*']
 ALLOWED_HOSTS = ['127.0.0.1','oubaidphone.onrender.com']

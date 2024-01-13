d872c74a88e577574bc408d3584ba79a0963b6db
bmhcode <bmhwin@gmail.com>
2024-01-13 22:47:55 +0100

    render

--- project/settings.py
+++ project/settings.py
@@ -57,8 +57,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    # "django.middleware.security.SecurityMiddleware",
-    # "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 
 ]
 

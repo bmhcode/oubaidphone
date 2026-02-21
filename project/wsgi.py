3ae50d1c7b035067f4841ed8ef7cc46122e6bde8
bmhcode <bmhwin@gmail.com>
2026-02-21 01:55:42 +0100

    config wsgi.py

--- project/wsgi.py
+++ project/wsgi.py
@@ -7,10 +7,24 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
+# import os
+
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+# application = get_wsgi_application()
+
+
+
 import os
+import sys
+
+path = '/home/bmhcodes/oubaidphone/project'
+if path not in sys.path:
+    sys.path.append(path)
+
+os.environ['DJANGO_SETTINGS_MODULE'] = 'project.settings'
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
-
 application = get_wsgi_application()

d7370926950f6506b98a0f615bbf25d7207bc5b0
bmhcode <bmhwin@gmail.com>
2026-02-24 06:47:32 +0100

    allowd_host

--- project/settings.py
+++ project/settings.py
@@ -25,8 +25,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 # DEBUG = True
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true' 
-# ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split(',')
+ALLOWED_HOSTS = ['*']
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS','').split(',')
 
 # Application definition
 INSTALLED_APPS = [
